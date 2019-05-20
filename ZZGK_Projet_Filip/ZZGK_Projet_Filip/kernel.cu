45be932abb0f23ff1fd93b81ff55fcfb4a340d4e
student <student@LAB-DBX-10>
2019-05-20 14:40:27 +0200

    progress

--- ZZGK_Projet_Filip/ZZGK_Projet_Filip/kernel.cu
+++ ZZGK_Projet_Filip/ZZGK_Projet_Filip/kernel.cu
@@ -1,121 +1,108 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/sort.h>
+#include <thrust/functional.h>
+#include<thrust/reduce.h>
 #include <stdio.h>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+const int size = 16;
+const int bits = 4;
+__global__ void joinGroups(int tab1[], int out[], int N) {
+	extern __shared__ int tab[];
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < N) {
+		tab[threadIdx.x] = tab1[idx];
+	}
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	_syncthreads();
+	if (idx < N) {
+		int a, b;
+		if (threadIdx.x > 0) {
+			a = tab[threadIdx.x - 1];
+			b = tab[threadIdx.x];
+		}
+		else if (idx > 0) {
+			a = tab1[idx - 1];
+			b = tab[threadIdx.x];
+		}
+		int dif = a ^ b;
+		if (__popc(dif) == 2&& dif > a&&dif > b) {
+				out[idx] = 0;
+		}
+		else {
+			out[idx] = 1;
+		}
+	}
 }
+
+struct isPositive {
+	__host__ __device__ bool operator()(int x) {
+		return x > 0;
+	}
+};
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	thrust::host_vector<int> h_tab1;
+	thrust::device_vector<int> d_tab1;
+	thrust::device_vector<int> d_tab2;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	std::vector<int> sizes;
+	int sum = 0;
+	while(sum < size){
+		int x = rand() % (size/2) + 1;
+		if (sum + x > size)
+			x = size - sum;
+		sizes.push_back(x);
+		sum += x;
+	}
+	int prefix = 0;
+	int plength=0;
+	for (int i = 1; i < bits;i++) {
+		prefix <<= 1;
+		prefix |= 1;
+		plength += 1;
+	}
+	int dif = 1 << (plength-1);
+	for (int x : sizes) {
+		unsigned a = 1 << 31;
+		for (int i = 0;i < x;i++) {
+			h_tab1.push_back(a | prefix);
+			a >>= 1;
+		}
+		prefix += dif;
+		if (dif >> (plength - 1) == 1) {
+			plength += 1;
+		}
+		dif >>= 1;
+		if (dif == 0) {
+			dif = 1 << (plength - 1);
+		}
+	}
+	d_tab1 = h_tab1;
+	d_tab2.resize(size);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	for (int i = 0;i < size;i++) {
+		printf("%x\n", h_tab1[i]);
+	}
+		printf("\n\n");
+	compare10 << <size / 64, 64, 64*sizeof(int) >> > (d_tab1.data().get(), d_tab2.data().get(), size);
+	thrust::copy_if(thrust::c (0),
+		thrust::make_counting_iterator(size),
+		d_tab2.begin(),
+		d_tab1.begin(),
+		thrust::identity<int>());
+	h_tab1 = d_tab1;
+	for (int i = 0;i < size;i++) {
+		printf("%d;", h_tab1[i]);
+	}
+	printf("\n\n");
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
+	printf("%f", (h_tab1[size / 2] + h_tab1[size / 2 + 1]) / 2.0);
+	*/
+	return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
